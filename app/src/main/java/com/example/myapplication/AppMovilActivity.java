ea65d6fb05d798233b5674a6c3e618ce1183b544
Abraham Huélamo <100346821@alumnos.uc3m.es>
2020-04-14 13:28:28 +0200

    Probando todo

--- app/src/main/java/com/example/myapplication/AppMovilActivity.java
+++ app/src/main/java/com/example/myapplication/AppMovilActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class AppMovilActivity extends AppCompatActivity {
 
+    //Pruebas
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
