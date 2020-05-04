265ffb496678ad09dee7413e4ddf5a6cda30c894
teresafe97 <100346811@alumnos.uc3m.es>
2020-05-04 18:10:20 +0200

    Holiiiii Cambios Teresa 4/5/20
    -BuscarRecetaActivity.java: Progress Dialog para cuando está buscando las recetas y Alert Dialog para cuando el usuario le da a buscar y no ha introducido nada en el buscador.
    -Bordes redondeados en plan_semanal.xml, principal.xml,perfil.xml, usuario.xml (no lo he cambiado en las vuestras por si acaso colisionaba, al final cambiaré las vuestras, o las cambiais vosotros si quereis).
    - UsuarioActivity.java y usuario.xml, implementación de las preferencias del nombre, edad, altura, y peso. En este layout también se ha cambiado la forma de seleccionar las intolerancias y los ingredientes, haciendolo ahora a través de un AlertDialog con Checkboxes. Una vez seleccionadas las intolerancias e ingredientes se muestra en un textView el resultado de la selección. Falta por guardar el spinner del sexo y los alertDialog de las intolerancias y alimentos.

--- app/src/main/java/com/example/myapplication/PerfilActivity.java
+++ app/src/main/java/com/example/myapplication/PerfilActivity.java
@@ -27,56 +27,52 @@ public class PerfilActivity extends AppCompatActivity {
         // Se obtiene el identificador del item que ha sido seleccionado
         int id = item.getItemId();
 
-        if (id == R.id.action_settings) {
-            System.out.println("APPMOV: About action...");
-            // Creamos el Intent que va a lanzar la segunda activity (SecondActivity)
-            Intent intent = new Intent(this, UsuarioActivity.class);
+        switch(id){
+            case R.id.action_settings:
+                System.out.println("APPMOV: About action...");
+                // Creamos el Intent que va a lanzar la segunda activity (SecondActivity)
+                Intent intentSettings = new Intent(this, UsuarioActivity.class);
 
-            // Iniciamos la nueva actividad
-            startActivity(intent);
-            return true;
-        }
-        if (id == R.id.action_about) {
-            System.out.println("APPMOV: About action...");
-            // Creamos el Intent que va a lanzar la segunda activity (SecondActivity)
-            Intent intent = new Intent(this, AppMovilActivity.class);
+                // Iniciamos la nueva actividad
+                startActivity(intentSettings);
+            break;
+            case R.id.action_about:
+                System.out.println("APPMOV: About action...");
+                // Creamos el Intent que va a lanzar la segunda activity (SecondActivity)
+                Intent intentAbout = new Intent(this, AppMovilActivity.class);
 
-            // Iniciamos la nueva actividad
-            startActivity(intent);
-            return true;
-        }
+                // Iniciamos la nueva actividad
+                startActivity(intentAbout);
+            break;
+            //NavBar
+            case R.id.action_favorite:
+                // Creamos el Intent que va a lanzar la segunda activity (SecondActivity)
+                Intent intentFav = new Intent(this,FavoritosActivity.class);
+                // Iniciamos la nueva actividad
+                startActivity(intentFav);
 
-        //NavBar
-        if (id == R.id.action_favorite) {
-            // Creamos el Intent que va a lanzar la segunda activity (SecondActivity)
-            Intent intent = new Intent(this,FavoritosActivity.class);
-            // Iniciamos la nueva actividad
-            startActivity(intent);
+                System.out.println("APPMOV: About action...");
+            break;
+            case R.id.action_fridge:
+                // Creamos el Intent que va a lanzar la segunda activity (SecondActivity)
+                Intent intentFridge = new Intent(this,NeveraActivity.class);
+                // Iniciamos la nueva actividad
+                startActivity(intentFridge);
 
-            System.out.println("APPMOV: About action...");
-            return true;
-        }
-        if (id == R.id.action_fridge) {
-            // Creamos el Intent que va a lanzar la segunda activity (SecondActivity)
-            Intent intent = new Intent(this,NeveraActivity.class);
-            // Iniciamos la nueva actividad
-            startActivity(intent);
+                System.out.println("APPMOV: About action...");
+            break;
 
-            System.out.println("APPMOV: About action...");
-            return true;
-        }
-        if (id == R.id.action_user) {
-            System.out.println("APPMOV: About action...");
-            return true;
-        }
-        if (id == R.id.action_search) {
-            // Creamos el Intent que va a lanzar la segunda activity (SecondActivity)
-            Intent intent = new Intent(this,BuscarRecetasActivity.class);
-            // Iniciamos la nueva actividad
-            startActivity(intent);
+            case R.id.action_user:
+                System.out.println("APPMOV: About action...");
+            break;
+            case R.id.action_search:
+                // Creamos el Intent que va a lanzar la segunda activity (SecondActivity)
+                Intent intentSearch = new Intent(this,BuscarRecetasActivity.class);
+                // Iniciamos la nueva actividad
+                startActivity(intentSearch);
 
-            System.out.println("APPMOV: About action...");
-            return true;
+                System.out.println("APPMOV: About action...");
+            break;
         }
 
         return super.onOptionsItemSelected(item);
