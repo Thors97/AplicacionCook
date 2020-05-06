d10809103cafa32b47a3b11d1a4c6c0c7c3c63d6
Abraham Huélamo <100346821@alumnos.uc3m.es>
2020-05-06 18:39:34 +0200

    Configuracion del BuscarRecetasAdapter y recuperacion de imagen de la respuesta

--- app/src/main/java/com/example/myapplication/Receta.java
+++ app/src/main/java/com/example/myapplication/Receta.java
@@ -1,10 +1,11 @@
 package com.example.myapplication;
 
+import android.graphics.Bitmap;
 import android.media.Image;
 
 public class Receta {
     private String nombre;
-    private Image imagen;
+    private Bitmap imagen;
     private String[] healthLabels;
     private String[] ingredientes;
     private String calorias;
@@ -12,17 +13,26 @@ public class Receta {
 
     public Receta(){
         this.nombre = "";
-        /*this.imagen = null;
-        this.healthLabels = null;
+        this.imagen = null;
+        /*this.healthLabels = null;
         this.ingredientes = null;
         this.calorias = "";
         this.tiempo = "";*/
     }
+
 
     public void setName(String nombre) {
         this.nombre = nombre;
     }
     public String getNombre() {
         return nombre;
+    }
+
+    public void setImagen(Bitmap imagen) {
+        this.imagen = imagen;
+    }
+
+    public Bitmap getImagen() {
+        return imagen;
     }
 }
